defdc16f7dfa19144af36d7585b2cefb4e696cc1
BANDISREEKUMAR <codewithsree5@gmail.com>
2024-11-17 14:37:43 +0530

    First commit

---
+++ Portfolio/script.js
@@ -0,0 +1,7 @@
+// Toggle Navigation Menu
+const navToggle = document.getElementById('nav-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+navToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
